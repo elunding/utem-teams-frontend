c69e0d74d517dc5a0babe16a9642a02a9da05ab0
Esteban Lundin <elundin@techk.cl>
2020-11-01 22:59:58 -0300

    logout added

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -6,15 +6,27 @@
           <router-link to="/projects" class="nav-link">Mis Proyectos</router-link>
         </li>
       </div>
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+            <a v-on:click.self="logout" class="nav-link">Cerrar Sesión</a>
+        </li>
+      </ul>
     </nav>
 </template>
 
 <script>
+    import UserAuthService from '../api/authentication.service'
+
     export default {
         name: 'NavBar',
         data() {
             return {};
+        },
+        methods: {
+            logout() {
+                UserAuthService.logout();
+                this.$router.push('/login');
+            }
         }
     }
-
 </script>
