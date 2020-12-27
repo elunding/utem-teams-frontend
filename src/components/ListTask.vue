c416f7bf1ebff9278f241cee7ba74e2fa01d918e
Esteban Lundin <elundin@techk.cl>
2020-12-27 16:06:04 -0300

    listTask component layout modified and draggable cards implemented

--- src/components/ListTask.vue
+++ src/components/ListTask.vue
@@ -3,58 +3,157 @@
     <h3> Tareas </h3>
     <br/>
     <div class="my-grid">
-      <div class="jumbotron" v-if="!tasks || !tasks.length">  
+      <div class="jumbotron" v-if="!tasksExists">  
         <h3 class="display-6">No hay tareas existentes</h3>
         <p class="lead"></p>
         <hr class="my-4">
         <p></p>
         <router-link to="tasks/new" tag="a" class="btn btn-primary btn-lg">Crear Tarea</router-link>
       </div>
-      <b-card-group v-else v-for="(task, index) in tasks" :key="index">
-          <b-card 
-            class="task-card"
-            :title="task.name"
-            style="max-width: 23rem; margin-bottom: 20px;"
-          >
-            <b-card-text class="task-description">
-              {{ task.description }}
-            </b-card-text>
-          </b-card>
-        </b-card-group>
+
+      <div class="row mt-3" v-else>
+        <!--<div class="col-sm-3">
+          <div class="p-2 alert alert-secondary">
+            <h4>Por hacer</h4>
+            <b-card-group  v-for="(task, index) in todoTasks" :key="index">
+              <b-card 
+                class="task-card"
+                :title="task.name"
+                style="max-width: 23rem; margin-bottom: 20px;"
+              >
+              <b-card-text class="task-description">
+                {{ task.description }}
+              </b-card-text>
+              </b-card>
+            </b-card-group>
+          </div>
+        </div>-->
+
+        <div class="col-sm-3">
+          <div class="p-3 alert alert-secondary card-group-cls">
+            <h4>Pendientes</h4>
+            <draggable class="card-group" v-for="(task, index) in todoTasks" :key="index">
+              <b-card 
+                class="task-card"
+                :title="task.name"
+                style="max-width: 23rem; margin-bottom: 20px;"
+              >
+              <b-card-text class="task-description">
+                {{ task.description }}
+              </b-card-text>
+              </b-card>
+            </draggable>
+          </div>
+        </div>
+
+        <div class="col-sm-3">
+          <div class="p-3 alert alert-primary card-group-cls">
+            <h4>En curso</h4>
+            <draggable class="card-group" v-for="(task, index) in inProgressTasks" :key="index">
+              <b-card 
+                class="task-card"
+                :title="task.name"
+                style="max-width: 23rem; margin-bottom: 20px;"
+              >
+              <b-card-text class="task-description">
+                {{ task.description }}
+              </b-card-text>
+              </b-card>
+            </draggable>
+          </div>
+        </div>
+
+        <!--<div class="col-sm-3">
+          <div class="p-2 alert alert-success">
+            <h4>Finalizadas</h4>
+            <b-card-group v-for="(task, index) in todoTasks" :key="index">
+              <b-card 
+                class="task-card"
+                :title="task.name"
+                style="max-width: 23rem; margin-bottom: 20px;"
+              >
+              <b-card-text class="task-description">
+                {{ task.description }}
+              </b-card-text>
+              </b-card>
+            </b-card-group>
+          </div>
+        </div>-->
+
+        <div class="col-sm-3">
+          <div class="p-3 alert alert-success card-group-cls">
+            <h4>Finalizadas</h4>
+            <draggable class="card-group" v-for="(task, index) in doneTasks" :key="index">
+              <b-card 
+                class="task-card"
+                :title="task.name"
+                style="max-width: 23rem; margin-bottom: 20px;"
+              >
+              <b-card-text class="task-description">
+                {{ task.description }}
+              </b-card-text>
+              </b-card>
+            </draggable>
+          </div>
+        </div>
+
+
       </div>
+
+      
+      
+    </div>
   </div>
 </template>
 
 <script>
 import { getTasks } from "../api/api.service.js";
+import draggable from "vuedraggable";
 
 export default {
   name: "list-task",
+  components: {
+    draggable
+  },
   data() {
     return {
-      tasks: []
+      todoTasks: [],
+      inProgressTasks: [],
+      doneTasks: [],
+      tasksExists: false
     };
   },
   methods: {
     retrieveTasks() {
       let projectId = this.$route.params.id;
-      console.log("projectId ", projectId);
-      console.log("calling getProjects...");
       getTasks(projectId)
         .then(response => {
-          this.tasks = response.data.data;
+          this.todoTasks = response.data.data.todo_tasks;
+          this.inProgressTasks = response.data.data.in_progress_tasks;
+          this.doneTasks = response.data.data.done_tasks;
+          this.$nextTick(() => this.setTasksExists(
+            this.todoTasks,
+            this.inProgressTasks,
+            this.doneTasks
+          ));
           console.log(response.data.data);
         })
         .catch(e => {
           console.log(e);
         });
     },
+    setTasksExists(todos, inProgress, done) {
+      if ((todos && todos.length) || (inProgress && inProgress.length) || (done && done.length)) {  
+        console.log("condition satisfied...");
+        this.tasksExists = true;
+      }
+    }
   },
   mounted() {
       console.log("mounted!")
-      console.log("calling retrieveTasks...")
+      console.log("calling retrieveTasks...");
       this.retrieveTasks();
-    }
+  },
 };
 </script>
 
@@ -67,17 +166,21 @@ export default {
     margin-bottom: 40px;
   }
 
-  .my-grid {
-    display: grid;
+  .card-group-cls {
+    min-height: 300px
+  }
+
+  /*.my-grid {
+    display: grid;*/
     /*justify-items: center;*/
     /* 280px is the minimum a column can get, you might need to adjust it based on your needs. */
-    grid-template-columns: repeat(auto-fit, minmax(280px, 1fr));
+    /*grid-template-columns: repeat(auto-fit, minmax(280px, 1fr));
     grid-gap: 1.5rem;
   }
 
   .my-grid > * {
     width: 100%;
     max-width: 20rem;
-  }
+  }*/
 
 </style>
